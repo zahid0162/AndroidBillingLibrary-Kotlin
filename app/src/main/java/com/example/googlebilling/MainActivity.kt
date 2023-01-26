d3098c45b1b5b8a2f980eb742806023db134fb14
“zahid0162” <zaid.muneer@ucp.edu.pk>
2023-01-26 11:31:43 +0500

    update

--- app/src/main/java/com/example/googlebilling/MainActivity.kt
+++ app/src/main/java/com/example/googlebilling/MainActivity.kt
@@ -2,21 +2,22 @@ package com.example.googlebilling
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.android.billingclient.api.*
 import com.example.googlebilling.databinding.ActivityMainBinding
+import com.google.firebase.crashlytics.buildtools.reloc.com.google.common.collect.ImmutableList
 
 class MainActivity : AppCompatActivity() {
     lateinit var viewBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewBinding=ActivityMainBinding.inflate(layoutInflater)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
         val skuList = ArrayList<String>()
         skuList.add("test_prod_1")
         skuList.add("test_prod_2")
         skuList.add("test_prod_3")
-        val purchasesUpdatedListener = PurchasesUpdatedListener{
-                billingResult, purchses ->
+        val purchasesUpdatedListener = PurchasesUpdatedListener { billingResult, purchses ->
         }
 
         var billingClient = BillingClient.newBuilder(this)
@@ -27,29 +28,30 @@ class MainActivity : AppCompatActivity() {
 
             billingClient.startConnection(object : BillingClientStateListener {
                 override fun onBillingServiceDisconnected() {
-//                    TODO("Not yet implemented")
+
                 }
 
                 override fun onBillingSetupFinished(billingResult: BillingResult) {
 //                    TODO("Not yet implemented")
 
-                    if (billingResult.responseCode == BillingClient.BillingResponseCode.OK){
+                    if (billingResult.responseCode == BillingClient.BillingResponseCode.OK) {
 
-                        val params = SkuDetailsParams.newBuilder()
-                        params.setSkusList(skuList)
-                            .setType(BillingClient.SkuType.INAPP)
-
-                        billingClient.querySkuDetailsAsync(params.build()){
-                                billingResult, skuDetailsList ->
-
-                            for (skuDetails in skuDetailsList!!) {
-                                val flowPurchase = BillingFlowParams.newBuilder()
-                                    .setSkuDetails(skuDetails)
-                                    .build()
-
-                                val responseCode = billingClient.launchBillingFlow(this@MainActivity, flowPurchase).responseCode
-                            }
+                        val queryProductDetailsParams =
+                            QueryProductDetailsParams.newBuilder()
+                                .setProductList(
+                                    ImmutableList.of(
+                                        QueryProductDetailsParams.Product.newBuilder()
+                                            .setProductId("product_id_example")
+                                            .setProductType(BillingClient.ProductType.SUBS)
+                                            .build()
+                                    )
+                                )
+                                .build()
+                        billingClient.queryProductDetailsAsync(queryProductDetailsParams) { billingResult,
+                                                                                            productDetailsList ->
+                            Toast.makeText(this@MainActivity,billingResult.toString(),Toast.LENGTH_SHORT).show()
                         }
+
                     }
                 }
 
